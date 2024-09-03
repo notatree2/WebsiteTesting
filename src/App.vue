444eddfc777501dafe148763d439801ed625c87c
CrOS Man <juliswater@gmail.com>
2024-09-02 20:17:11 -0400

    lets try this again tomorrow

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <script setup>
+    import { ref, onMounted} from "vue"
+    import { useDark } from "@vueuse/core"
+    import projects from "./components/projects.vue"
+    const source = ref("https://tech-independence-journey.codeberg.page/")
+
 
 </script>
 
 <template>
+    <div class="h-screen max-w-full bg-gradient-to-r from-slate-800 to-slate-800">
+        <div class="h-screen text-center mr-48 ml-48">
+            <div class=" p-10 text-pretty text-slate-900 space-y-2">
+                <h1 class="font-bold text-4xl ">Hello, i am <span class="text-blue-500">notacupofmilk.</span></h1>
+                <p class="text-3xl">I am a <span class="text-yellow-500">Christian</span>, a guy who play video games, and i like the internet!</p>
+
+
+                <br>
+                <br>
+                <br>
+
+                
+            <projects/>
+                
+                
+            </div>
+
+        </div>
+
+    </div>
 
 </template>
 
---
+++ src/components/projects.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { ref, onMounted} from "vue"
+    import { useDark } from "@vueuse/core"
+    const source = ref("https://tech-independence-journey.codeberg.page/")
+
+
+</script>
+
+<template>
+
+
+                <h1 class="font-semibold text-3xl">Projects</h1>
+                <!-- project grid -->
+                 <br>
+                 <div class="grid grid-cols-2 group gap-3 ">
+                    
+                    <!-- button -->
+                    
+                    <button class="group-active:bg-slate-500 p-5 font-semibold  rounded-lg shadow-lg bg-slate-700 transition-all group-hover:bg-slate-600 " :href="source">Tech Independence Journey</button>
+                
+                </div>
+
+</template>
+
+<style scoped>
+
+</style>
