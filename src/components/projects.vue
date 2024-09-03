532f1201677d243ee4c5e201e40853c8e0bc9081
CrOS Man <juliswater@gmail.com>
2024-09-02 20:20:55 -0400

    Button now fixed

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -12,11 +12,11 @@
                 <h1 class="font-semibold text-3xl">Projects</h1>
                 <!-- project grid -->
                  <br>
-                 <div class="grid grid-cols-2 group gap-3 ">
+                 <div class="grid grid-cols-2 gap-3 grid-rows-2">
                     
                     <!-- button -->
                     
-                    <button class="group-active:bg-slate-500 p-5 font-semibold  rounded-lg shadow-lg bg-slate-700 transition-all group-hover:bg-slate-600 " :href="source">Tech Independence Journey</button>
+                    <button class="active:bg-slate-500 p-5 font-semibold  rounded-lg shadow-lg bg-slate-700 transition-all hover:bg-slate-600 " :href="source">Tech Independence Journey</button>
                 
                 </div>
 
